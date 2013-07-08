db5ba5bcfa2db6a728f0d25b1589d4a79dc70aa9
nicon8 <nicola.manica@gmail.com>
2013-07-08 13:01:22 +0200

    Json Ecample: add icon download

--- Day3/JsonExample/src/com/ssict/jsonexample/ParserActivity.java
+++ Day3/JsonExample/src/com/ssict/jsonexample/ParserActivity.java
@@ -1,5 +1,8 @@
 package com.ssict.jsonexample;
 
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 import it.ssict.jsonparser.R;
 
 import org.json.JSONException;
@@ -7,21 +10,27 @@ import org.json.JSONObject;
 
 import com.google.gson.Gson;
 import com.ssict.jsonexample.utils.JSONParser;
+import com.ssict.jsonexample.weatherdata.Condition;
 import com.ssict.jsonexample.weatherdata.LocalData;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class ParserActivity extends Activity {
 
+	LocalData data;
+	ImageView image;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_parser);
-		pippo();
+		image = (ImageView) findViewById(R.id.imageView1);
+		getJson();
 	}
 
 	@Override
@@ -35,7 +44,7 @@ public class ParserActivity extends Activity {
 	
 	
 	
-	public void pippo() {
+	public void getJson() {
 		// Creating JSON Parser instance
 		
 		 
@@ -49,7 +58,7 @@ public class ParserActivity extends Activity {
 			protected Boolean doInBackground(String... arg0) {
 				// TODO Auto-generated method stub
 				JSONParser jParser = new JSONParser(ParserActivity.this);
-				String url = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=-25.948166,32.618408&key=p7ptew8qk9wfhcsspv8zvuq9&format=json&includeLocation=yes&num_of_days=10";
+				String url = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=-25.948166,32.618408&key=p7ptew8qk9wfhcsspv8zvuq9&format=json&includeLocation=yes&num_of_days=1";
 				JSONObject json = jParser.getJSONFromUrl(url);
 				
 				Gson gson = new Gson();
@@ -59,41 +68,35 @@ public class ParserActivity extends Activity {
 		 		} catch (JSONException e) {
 		 			e.printStackTrace();
 		 		}		        
-		        LocalData data = gson.fromJson(dataObject.toString(), LocalData.class);
-				Log.d("END",data+"");					
+		        data = gson.fromJson(dataObject.toString(), LocalData.class);				
+				
+				for (Condition c : data.currentCondition)
+				{
+					try {
+					    URL thumb_u = new URL(c.weatherIconUrl.get(0).value);
+					    Drawable thumb_d = Drawable.createFromStream(thumb_u.openStream(), "src");
+					    image.setImageDrawable(thumb_d);
+					}
+					catch (Exception e) {
+					    e.printStackTrace();
+					}
+					
+				}
 				return true;
 			}
 			
 		};
-		task.execute("");
+		try {
+			task.execute("").get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		Log.d("das","END");
-		/*try {
-		    // Getting Array of Contacts
-		    //contacts = json.getJSONArray(TAG_CONTACTS);
-		     
-		    // looping through All Contacts
-		    for(int i = 0; i < contacts.length(); i++){
-		        JSONObject c = contacts.getJSONObject(i);
-		         
-		        // Storing each json item in variable
-		        String id = c.getString(TAG_ID);
-		        String name = c.getString(TAG_NAME);
-		        String email = c.getString(TAG_EMAIL);
-		        String address = c.getString(TAG_ADDRESS);
-		        String gender = c.getString(TAG_GENDER);
-		         
-		        // Phone number is agin JSON Object
-		        JSONObject phone = c.getJSONObject(TAG_PHONE);
-		        String mobile = phone.getString(TAG_PHONE_MOBILE);
-		        String home = phone.getString(TAG_PHONE_HOME);
-		        String office = phone.getString(TAG_PHONE_OFFICE);
-		         
-		    
-		} catch (JSONException e) {
-		    e.printStackTrace();
-		}*/
-		
-		
 		
 	}
 	
